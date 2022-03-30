17298973480c6205de2a3d3b9a9257aa7c754960
YunoGH <schmalzlnico@gmail.com>
2022-03-30 20:26:57 +0200

    commented article class

--- src/main/java/ac/at/fhcampuswien/Article.java
+++ src/main/java/ac/at/fhcampuswien/Article.java
@@ -1,35 +1,44 @@
 package ac.at.fhcampuswien;
 
 public class Article {
+//creates a template for an article
 
     private String author;
     private String title;
 
-    public Article() {
+    //empty constructor
+    public Article(){
 
     }
 
+    //creates an author and a title for the article class
     public Article(String author, String title) {
         this.author = author;
         this.title = title;
     }
 
+    //setter- & getter methods
+    //sets a new author variable
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    //sets a new title variable
     public void setTitle(String title) {
         this.title = title;
     }
 
+    //returns a new author variable
     public String getAuthor() {
         return author;
     }
 
+    //returns a new title variable
     public String getTitle() {
         return title;
     }
 
+    //overrrides the toString method
     @Override
     public String toString() {
         return "Title: " + title + "\nAuthor: " + author + " ";
