07307cb93fa8676a961326b445496876520c46e6
05DNZL20 <92020567+05DNZL20@users.noreply.github.com>
2022-06-24 18:07:23 +0200

    Safa: Design Patterns

--- src/main/java/ac/at/fhcampuswien/classes/Article.java
+++ src/main/java/ac/at/fhcampuswien/classes/Article.java
@@ -12,16 +12,16 @@ public class Article {
     private String content;
     private Source source;
 
-    //creates an author and a title for the article class
-    public Article(Source source, String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
-        this.source = source;
-        this.author = author;
-        this.title = title;
-        this.description = description;
-        this.url = url;
-        this.urlToImage = urlToImage;
-        this.publishedAt = publishedAt;
-        this.content = content;
+    //creates an Article with the a Builder
+    public Article(Builder builder) {
+        this.author = builder.author;
+        this.title = builder.title;
+        this.description = builder.description;
+        this.url = builder.url;
+        this.urlToImage = builder.urlToImage;
+        this.publishedAt = builder.publishedAt;
+        this.content = builder.content;
+        this.source = builder.source;
     }
 
     //returns a new author variable
@@ -58,6 +58,59 @@ public class Article {
         return source;
     }
 
-    public void setDescription(String description) {this.description = description;
+    public void setDescription(String description) {this.description = description;}
+
+    //Builder class
+    public static class Builder{
+        //must variables
+        private final Source source;
+        private final String title;
+
+        //optional variables
+        private String author;
+        private String description;
+        private String url;
+        private String urlToImage;
+        private String publishedAt;
+        private String content;
+
+        //Constructor for the must variables
+        public Builder(Source source, String title){
+            this.source = source;
+            this.title = title;
+        }
+
+        //Constructor for the optional variables
+        public Builder author(String author){
+            this.author = author;
+            return this;
+        }
+        public Builder description(String description){
+            this.description = description;
+            return this;
+        }
+        public Builder url(String url){
+            this.url = url;
+            return this;
+        }
+        public Builder urlToImage(String urlToImage){
+            this.urlToImage = urlToImage;
+            return this;
+        }
+        public Builder publishedAt(String publishedAt){
+            this.publishedAt = publishedAt;
+            return this;
+        }
+        public Builder content(String content){
+            this.content = content;
+            return this;
+        }
+
+        //builds the Article
+        public Article build(){
+            return new Article(this);
+        }
+
+
     }
 }
