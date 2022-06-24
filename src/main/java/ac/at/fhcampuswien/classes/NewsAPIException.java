87173d260d8fdaf93cefa8daa55c4c9540d0902d
YunoGH <89008852+YunoGH@users.noreply.github.com>
2022-06-24 19:18:21 +0200

    added downlaoder class and small changes in NewsAPIException

--- src/main/java/ac/at/fhcampuswien/classes/NewsAPIException.java
+++ src/main/java/ac/at/fhcampuswien/classes/NewsAPIException.java
@@ -1,7 +1,19 @@
-package ac.at.fhcampuswien.classes;
+/*package ac.at.fhcampuswien.classes;
 
 public class NewsAPIException extends RuntimeException{
     NewsAPIException(){
         super("Exceeded amount of requests!\nYou consumed the amount of requests for 2 API-KEYS!!!");
+    }
+}
+*/
+
+package ac.at.fhcampuswien.classes;
+
+public class NewsAPIException extends RuntimeException{
+    public NewsAPIException() {
+        super();
+    }
+    public NewsAPIException(String message) {
+        super(message);
     }
 }
